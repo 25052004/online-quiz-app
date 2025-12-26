a871dd73c7bfb387895dfd9d9f57f008b8d8d1ad
Om Gupta <omgupta8957123@gmail.com>
2025-12-26 21:27:28 +0530

    Fix Render port

--- backend/server.js
+++ backend/server.js
@@ -26,9 +26,7 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes"));
 
 // Start Server
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
-});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
