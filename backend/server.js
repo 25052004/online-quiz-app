4a7775c759fab53170cf5fde158c1c72fbe25ab1
Om Gupta <omgupta8957123@gmail.com>
2025-12-27 08:46:21 +0530

    Fix auth register route

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,7 @@ app.use(express.json());
 
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
+  
 
 .then(() => {
     console.log("MongoDB Connected Successfully");
@@ -26,7 +27,9 @@ app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/quiz", require("./routes/quizRoutes"));
 
 // Start Server
-
+app.listen(5000, () => {
+    console.log("Server running on port 5000");
+});
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
